b83736cfa14875b4c7874c6030f3291420723cd5
WAMBUINGUNGA <wambuiregina28@gmail.com>
2024-08-08 11:15:41 +0300

    sideBar

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,52 +1,86 @@
-// Sidebar.jsx
 import React, { useContext } from 'react';
-import { FiHome, FiUser, FiFilePlus, FiBriefcase, FiBookmark, FiDollarSign, FiUsers, FiSettings } from 'react-icons/fi';
+import {  FiBriefcase, FiDollarSign, FiUsers, FiSettings } from 'react-icons/fi';
 import { UserContext } from '../context/UserContext';
 import { CgProfile } from "react-icons/cg";
 import { NavLink } from 'react-router-dom';
+import { FaHome } from "react-icons/fa";
+import { RiProgress3Fill } from "react-icons/ri";
+import { BiSolidAddToQueue } from "react-icons/bi";
+import { TiUserAdd } from "react-icons/ti";
+import { TbBriefcaseFilled } from "react-icons/tb";
+
+
+
+
 
 const Sidebar = () => {
   const { currentUser } = useContext(UserContext);
+
   return (
     <div className="hidden md:block w-[20vw] h-[90vh] fixed top-[10vh] z-50 bg-gradient-to-b from-blue-300 to-blue-200 border-t-gray-300 border-2 shadow-md">
       {currentUser && currentUser.username && (
-        <>
-          <div className='flex flex-col items-center justify-center mt-4'>
-            {currentUser.avatar ? (
-              <img src={currentUser.avatar} alt={currentUser.firstname} className='h-20 w-20 rounded-full border-2 border-white object-cover top-0' />
-            ) : (
-              <CgProfile className='h-16 w-16 text-black' />
-            )}
-            <h2 className="text-xl font-semibold mt-2 text-black capitalize">{currentUser.firstname} {currentUser.lastname}</h2>
-          </div>
-        </>
+        <div className='flex flex-col items-center justify-center mt-4'>
+          {currentUser.avatar ? (
+            <img src={currentUser.avatar} alt={currentUser.firstname} className='h-16 w-16 rounded-full border-2 border-white object-cover top-0 ring-1 ring-[#3322ca]' />
+          ) : (
+            <CgProfile className='h-16 w-16 text-black' />
+          )}
+          <h2 className="mt-2 text-center text-2xl font-semibold leading-9 tracking-tight text-gray-900 capitalize">
+            {currentUser.firstname} {currentUser.lastname}
+          </h2>
+        </div>
       )}
       <nav className="mt-4">
         <ul>
-          <NavLink to="/client" className="p-4 hover:bg-gray-200 flex items-center" activeClassName="bg-white">
-            <FiHome className="mr-2" /> Overview
+          
+
+        {/* CLIENT */}
+
+        {currentUser && currentUser.is_client && (
+            <>
+            <NavLink
+            to="/client"
+            end
+            className={({ isActive }) => p-4 flex items-center text-xl font-semibold ${isActive ? 'bg-gray-300' : 'hover:bg-gray-200'}}
+          >       
+            <FaHome className="mr-2" /> Overview
           </NavLink>
-          <NavLink to="/client/update-profile" className="p-4 hover:bg-gray-200 flex items-center" activeClassName="bg-gray-300">
-            <FiUser className="mr-2" /> Employers Profile
+            <NavLink
+            to="/client/update-profile"
+            className={({ isActive }) => p-4 flex items-center text-xl font-semibold ${isActive ? 'bg-gray-300' : 'hover:bg-gray-200'}}
+          >
+            <TiUserAdd className="mr-2" /> Profile
           </NavLink>
-          <NavLink to="/client/create-job" className="p-4 hover:bg-gray-200 flex items-center" activeClassName="bg-gray-300">
-            <FiFilePlus className="mr-2" /> Post a Job
+          <NavLink
+            to="/client/create-job"
+            className={({ isActive }) => p-4 flex items-center text-xl font-semibold ${isActive ? 'bg-gray-300' : 'hover:bg-gray-200'}}
+          >
+            <BiSolidAddToQueue className='mr-2'/>  Post a Job           
           </NavLink>
-          <NavLink to="/client/my-jobs" className="p-4 hover:bg-gray-200 flex items-center" activeClassName="bg-gray-300">
+          <NavLink
+            to="/client/my-jobs"
+            className={({ isActive }) => p-4 flex items-center text-xl font-semibold ${isActive ? 'bg-gray-300' : 'hover:bg-gray-200'}}
+          >
             <FiBriefcase className="mr-2" /> My Jobs
           </NavLink>
-          <NavLink to="/client/freelancers" className="p-4 hover:bg-gray-200 flex items-center" activeClassName="bg-gray-300">
-            <FiBookmark className="mr-2" /> Saved Candidates
+          <NavLink
+            to="/client/progress"
+            className={({ isActive }) => p-4 flex items-center text-xl font-semibold ${isActive ? 'bg-gray-300' : 'hover:bg-gray-200'}}
+          >
+            <RiProgress3Fill className="mr-2"  />
+          Progress
           </NavLink>
-          <NavLink to="/client/plans-billing" className="p-4 hover:bg-gray-200 flex items-center" activeClassName="bg-gray-300">
-            <FiDollarSign className="mr-2" /> Plans & Billing
+          <NavLink
+            to="/client/freelancers"
+            className={({ isActive }) => p-4 flex items-center text-xl font-semibold ${isActive ? 'bg-gray-300' : 'hover:bg-gray-200'}}
+          >
+             <FiUsers className="mr-3 text-2xl" /> Freelancers
           </NavLink>
-          <NavLink to="/client/all-companies" className="p-4 hover:bg-gray-200 flex items-center" activeClassName="bg-gray-300">
-            <FiUsers className="mr-2" /> All Companies
-          </NavLink>
-          <NavLink to="/client/settings" className="p-4 hover:bg-gray-200 flex items-center" activeClassName="bg-gray-300">
-            <FiSettings className="mr-2" /> Settings
-          </NavLink>
+              
+            </>
+          )}     
+               
+
         </ul>
       </nav>
     </div>
